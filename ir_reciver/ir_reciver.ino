89514a44f909883ee3006d256c2cc528338c3d9d
Mike <mike.blank@lantia.de>
2019-11-25 20:22:53 +0100

    TASK: created a accelerometer folder & addet a poti routine to the ir_reciver

--- ir_reciver/ir_reciver.ino
+++ ir_reciver/ir_reciver.ino
@@ -1,6 +1,7 @@
 #include <MIDI.h>
 
 #define ir A0
+#define poti A1
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
@@ -8,6 +9,9 @@ int ir_akt = 0;
 int ir_map_akt = 0;
 int ir_map_last = 0;
 int ir_sum = 0;
+
+int poti_akt = 0;
+int poti_last = 0;
 
 void setup()
 {
@@ -17,6 +21,7 @@ void setup()
 
 void loop()
 {
+    //code for ir sens
     ir_sum = 0;
     for(int i=0; i<100; i++) {
         ir_sum = ir_sum+float(analogRead(ir));
@@ -27,8 +32,15 @@ void loop()
         ir_map_akt = map(ir_akt, 100, 320, 0, 127);
     }
 
-    if(ir_map_akt - ir_map_last > 3 || ir_map_last - ir_map_akt > 3) {
+    if(ir_map_akt - ir_map_last > 6 || ir_map_last - ir_map_akt > 6) {
         ir_map_last = ir_map_akt;
         MIDI.sendNoteOn(ir_map_akt, 127, 1);
+    }
+
+    //code for poti
+    poti_akt = analogRead(poti);
+    if(poti_akt - poti_last > 10 || poti_last - poti_akt > 10) {
+        MIDI.sendNoteOn(map(poti_akt, 0, 1023, 0, 127), 127, 2);
+        poti_last = poti_akt;
     }
 }
