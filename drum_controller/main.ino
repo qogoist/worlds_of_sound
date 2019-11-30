90a4fe03c7a949058dff142f42519e416eb53e6d
Jonas Haller <jonas.haller.jh@gmail.com>
2019-11-30 11:36:50 +0100

    added basic drumroll routine & changed names for existing controllers
    To match new naming scheme, should always be "[type of controller]_controller"

---
+++ drum_controller/main.ino
@@ -0,0 +1,40 @@
+#include <NewPing.h>
+#include <MIDI.h>
+
+#define SONAR_NUM 4
+#define MAX_DISTANCE 50
+#define multiplier 2
+
+NewPing sonar[SONAR_NUM] = {
+    NewPing(2, 3, MAX_DISTANCE),
+    NewPing(6, 5, MAX_DISTANCE),
+    NewPing(8, 9, MAX_DISTANCE),
+    NewPing(12, 11, MAX_DISTANCE)};
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+int distance = 0;
+
+void setup()
+{
+    MIDI.begin();
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    for (int i = 0; i < SONAR_NUM; i++)
+    {
+        distance = sonar[i].ping_cm();
+
+        if (distance != 0)
+        {
+            /*Serial.print("Sensor ");
+            Serial.print(i);
+            Serial.print(": ");
+            Serial.println(distance);*/
+
+            MIDI.sendNoteOn(i+1, 127, 10);
+        }
+    }
+}
