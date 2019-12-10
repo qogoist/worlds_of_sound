96e0c3a466cecca2f35c97bd335b7499a6337adc
Jonas Haller <jonas.haller.jh@gmail.com>
2019-12-10 11:57:36 +0100

    Updated drum controller to act as switches

--- drum_controller/main.ino
+++ drum_controller/main.ino
@@ -15,6 +15,16 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 int distance = 0;
 
+bool side1 = false;
+bool side2 = false;
+bool side3 = false;
+bool side4 = false;
+
+bool isSending1 = false;
+bool isSending2 = false;
+bool isSending3 = false;
+bool isSending4 = false;
+
 void setup()
 {
     MIDI.begin();
@@ -27,15 +37,96 @@ void loop()
     {
         delay(30);
         distance = sonar[i].ping_cm();
+        //Serial.print("Distance: ");
+        //Serial.println(distance);
 
-        if (distance != 0)
+        if (distance > 0)
         {
-            // Serial.print("Sensor ");
-            // Serial.print(i);
-            // Serial.print(": ");
-            // Serial.println(distance);
+            switch (i + 1)
+            {
+            case 1:
+                if (!isSending1)
+                    side1 = !side1;
 
-            MIDI.sendNoteOn(i+1, 127, 10);
+                if (side1)
+                    MIDI.sendNoteOn(i + 1, 127, 10);
+                //Serial.println(i + 1);
+                else
+                    MIDI.sendNoteOn(5 * (i + 1), 127, 10);
+                //Serial.println(5 * (i + 1));
+
+                isSending1 = true;
+                break;
+
+            case 2:
+                if (!isSending2)
+                    side2 = !side2;
+
+                if (side2)
+                    MIDI.sendNoteOn(i + 1, 127, 10);
+                //Serial.println(i + 1);
+                else
+                    MIDI.sendNoteOn(5 * (i + 1), 127, 10);
+                //Serial.println(5 * (i + 1));
+
+                isSending2 = true;
+                break;
+
+            case 3:
+                if (!isSending3)
+                    side3 = !side3;
+
+                if (side3)
+                    MIDI.sendNoteOn(i + 1, 127, 10);
+                //Serial.println(i + 1);
+                else
+                    MIDI.sendNoteOn(5 * (i + 1), 127, 10);
+                //Serial.println(5 * (i + 1));
+
+                isSending3 = true;
+                break;
+
+            case 4:
+                if (!isSending4)
+                    side4 = !side4;
+
+                if (side4)
+                    MIDI.sendNoteOn(i + 1, 127, 10);
+                //Serial.println(i + 1);
+                else
+                    MIDI.sendNoteOn(5 * (i + 1), 127, 10);
+                //Serial.println(5 * (i + 1));
+
+                isSending4 = true;
+                break;
+
+            default:
+                break;
+            }
+        }
+        else
+        {
+            switch (i + 1)
+            {
+            case 1:
+                isSending1 = false;
+                break;
+
+            case 2:
+                isSending2 = false;
+                break;
+
+            case 3:
+                isSending3 = false;
+                break;
+
+            case 4:
+                isSending4 = false;
+                break;
+
+            default:
+                break;
+            }
         }
     }
 }
