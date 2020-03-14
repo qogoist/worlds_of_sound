761b1e12b8813157e71c8de49146078fc1135ecd
Jonas Haller <jonas.haller.jh@gmail.com>
2020-03-14 15:23:48 +0100

    Added continuous cube

--- cube_continuous_controller/main.ino
+++ cube_continuous_controller/main.ino
@@ -1,34 +1,32 @@
-//both libs can be found in the Arduino I2C dev libs, reffer REDME
 #include "I2Cdev.h"
 #include "MPU6050.h"
-
-//the Arduino MIDI lib
 #include <MIDI.h>
 
-//the standard Arduino I2C lib, dosen't need to be downloadet seperadly
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
 
-//orientation detection threshold
-const int thr_x_plus = 15000;
-const int thr_x_minus = -15000;
-const int thr_y_plus = 15000;
-const int thr_y_minus = -15000;
-const int thr_z_plus = 15000;
-const int thr_z_minus = -15000;
+#include<OSCBundle.h>
+#include<OSCBoards.h>
+
+#ifdef BOARD_HAS_USB_SERIAL
+#include <SLIPEncodedUSBSerial.h>
+SLIPEncodedUSBSerial SLIPSerial( thisBoardsSerialUSB );
+#else
+#include <SLIPEncodedSerial.h>
+ SLIPEncodedSerial SLIPSerial(Serial); // Change to Serial1 or Serial2 etc. for boards with multiple serial ports that don’t have Serial
+#endif
 
 int side = 0;
 
-//initialize the on the standard I2C adress of the MPU6050
 MPU6050 accelgyro;
 
-//initialize the MIDI lib
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-//Orientation and motion variables
 int16_t ax, ay, az;
-int16_t gx, gy, gz;
+double length;
+double fx, fy, fz;
+int s1, s2, s3, s4, s5, s6;
 
 void setup()
 {
@@ -39,56 +37,59 @@ void setup()
     Fastwire::setup(400, true);
 #endif
 
-    // initialize serial communication
-    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-    // it's really up to you depending on your project)
-    Serial.begin(115200);
-
-    // initialize device
-    Serial.println("Initializing I2C devices...");
+    SLIPSerial.begin(38400);
     accelgyro.initialize();
-
-    // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-    // use the code below to change accel/gyro offset values
-    
-    Serial.println("Updating internal sensor offsets...");
-    // -76	-2359	1688	0	0	0
-    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
-    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
-    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
-    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    accelgyro.setXGyroOffset(220);
-    accelgyro.setYGyroOffset(76);
-    accelgyro.setZGyroOffset(-85);
-    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
-    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
-    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
-    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    
+    accelgyro.setXAccelOffset(472);
+    accelgyro.setYAccelOffset(350);
+    accelgyro.setZAccelOffset(1344);
 }
 
 void loop()
 {
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    accelgyro.getAcceleration(&ax, &ay, &az);
 
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    // accelgyro.getRotation(&gx, &gy, &gz);
+    length = sqrt(pow(ax, 2) + pow(ay, 2) + pow(az, 2));
 
-    Serial.print("aX: "); Serial.print(ax); Serial.print("\t");
-    Serial.print("aY: "); Serial.print(ay); Serial.print("\t");
-    Serial.print("aZ: "); Serial.print(az); Serial.print("\t");
-    Serial.print("gX: "); Serial.print(gx); Serial.print("\t");
-    Serial.print("gY: "); Serial.print(gy); Serial.print("\t");
-    Serial.print("gZ: "); Serial.println(gz);
+    fx = ax / length;
+    fy = ay / length;
+    fz = az / length;
+
+    s1 = round(mapValue(fy, 1, -1, 0, 100));
+    s6 = round(mapValue(fy, -1, 1, 0, 100));
+    s2 = round(mapValue(fz, 1, -1, 0, 100));
+    s5 = round(mapValue(fz, -1, 1, 0, 100));
+    s4 = round(mapValue(fx, 1, -1, 0, 100));
+    s3 = round(mapValue(fx, -1, 1, 0, 100));
+
+    OSCBundle bndl;
+    
+    bndl.add("/cube1/side1").add(s1);
+    bndl.add("/cube1/side2").add(s2);
+    bndl.add("/cube1/side3").add(s3);
+    bndl.add("/cube1/side4").add(s4);
+    bndl.add("/cube1/side5").add(s5);
+    bndl.add("/cube1/side6").add(s6);
+
+
+    SLIPSerial.beginPacket();  
+        bndl.send(SLIPSerial);
+    SLIPSerial.endPacket(); 
+    bndl.empty();
+    
+    delay(20);
+
+    // Serial.print("X: "); Serial.print(fx); Serial.print("\t");
+    // Serial.print("Y: "); Serial.print(fy); Serial.print("\t");
+    // Serial.print("Z: "); Serial.print(fz); Serial.print("\t");
+    // Serial.print("s1: "); Serial.print(s1); Serial.print("\t");
+    // Serial.print("s6: "); Serial.print(s6); Serial.print("\t");
+    // Serial.print("s2: "); Serial.print(s2); Serial.print("\t");
+    // Serial.print("s5: "); Serial.print(s5); Serial.print("\t");
+    // Serial.print("s3: "); Serial.print(s3); Serial.print("\t");
+    // Serial.print("s4: "); Serial.print(s4); Serial.print("\n");
+}
+
+double mapValue(double value, double in_min, double in_max, double out_min, double out_max)
+{
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
