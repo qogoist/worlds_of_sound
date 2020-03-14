354294fc3edf5d43975979f3f76b1fc09fee0b72
Jonas Haller <jonas.haller.jh@gmail.com>
2020-03-14 11:31:11 +0100

    updated continuous to testing stage

--- cube_continuous_controller/main.ino
+++ cube_continuous_controller/main.ino
@@ -26,83 +26,69 @@ MPU6050 accelgyro;
 //initialize the MIDI lib
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-//initialize vars for accelerometer values
-int16_t acc_x, acc_y, acc_z;
+//Orientation and motion variables
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
 void setup()
 {
-//start I2C communication
+// join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     Wire.begin();
 #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
     Fastwire::setup(400, true);
 #endif
 
-    //stat MIDI communication
-    MIDI.begin();
-
-    //start serial communication
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
     Serial.begin(115200);
 
-    //initialize accelerometer
+    // initialize device
+    Serial.println("Initializing I2C devices...");
     accelgyro.initialize();
 
-    //verify connection
+    // verify connection
+    Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    // use the code below to change accel/gyro offset values
+    
+    Serial.println("Updating internal sensor offsets...");
+    // -76	-2359	1688	0	0	0
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+    accelgyro.setXGyroOffset(220);
+    accelgyro.setYGyroOffset(76);
+    accelgyro.setZGyroOffset(-85);
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+    
 }
 
 void loop()
 {
-    //get acceleration data
-    accelgyro.getAcceleration(&acc_x, &acc_y, &acc_z);
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    // Serial.print(acc_x); Serial.print(" \t ");
-    // Serial.print(acc_y); Serial.print(" \t ");
-    // Serial.println(acc_z);
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    // accelgyro.getRotation(&gx, &gy, &gz);
 
-    if (acc_x < thr_x_plus && acc_x > thr_x_minus && acc_y < thr_y_plus && acc_y > thr_y_minus && acc_z > thr_z_plus && acc_z > thr_z_minus)
-    { //trigger if 1 is up
-        // Serial.println("1");
-        isSide(1);
-    }
-
-    if (acc_x > thr_x_plus && acc_x > thr_x_minus && acc_y < thr_y_plus && acc_y > thr_y_minus && acc_z < thr_z_plus && acc_z > thr_z_minus)
-    { //trigger if 2 is up
-        // Serial.println("2");
-        isSide(2);
-    }
-
-    if (acc_x < thr_x_plus && acc_x > thr_x_minus && acc_y < thr_y_plus && acc_y > thr_y_minus && acc_z < thr_z_plus && acc_z < thr_z_minus)
-    { //trigger if 3 is up
-        // Serial.println("3");
-        isSide(3);
-    }
-
-    if (acc_x < thr_x_plus && acc_x < thr_x_minus && acc_y < thr_y_plus && acc_y > thr_y_minus && acc_z < thr_z_plus && acc_z > thr_z_minus)
-    { //trigger if 4 is up
-        // Serial.println("4");
-        isSide(4);
-    }
-
-    if (acc_x < thr_x_plus && acc_x > thr_x_minus && acc_y > thr_y_plus && acc_y > thr_y_minus && acc_z < thr_z_plus && acc_z > thr_z_minus)
-    { //trigger if 5 is up
-        // Serial.println("5");
-        isSide(5);
-    }
-
-    if (acc_x < thr_x_plus && acc_x > thr_x_minus && acc_y < thr_y_plus && acc_y < thr_y_minus && acc_z < thr_z_plus && acc_z > thr_z_minus)
-    { //trigger if 6 is up
-        // Serial.println("6");
-        isSide(6);
-    }
-}
-
-bool isSide(int currentSide)
-{
-    if (side != currentSide)
-    {
-        MIDI.sendNoteOff(60, 0, 1);
-        side = currentSide;
-        MIDI.sendNoteOn(60, side * 20 - 10, 1); //Should send 10, 30, 50, 70, 90, 110, depending on side 1-6
-    }
+    Serial.print("aX: "); Serial.print(ax); Serial.print("\t");
+    Serial.print("aY: "); Serial.print(ay); Serial.print("\t");
+    Serial.print("aZ: "); Serial.print(az); Serial.print("\t");
+    Serial.print("gX: "); Serial.print(gx); Serial.print("\t");
+    Serial.print("gY: "); Serial.print(gy); Serial.print("\t");
+    Serial.print("gZ: "); Serial.println(gz);
 }
